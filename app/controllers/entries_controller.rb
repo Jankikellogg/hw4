1c14effbf3dbe8e73674de88ce6177b1d549b089
Jankikellogg <janki.somani@kellogg.northwestern.edu>
2026-06-11 23:36:26 +0000

    Fix entries controller class

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,21 +1,28 @@
-def create
+class EntriesController < ApplicationController
 
-  @entry = Entry.new
-
-  @entry["title"] = params["title"]
-  @entry["description"] = params["description"]
-  @entry["occurred_on"] = params["occurred_on"]
-  @entry["place_id"] = params["place_id"]
-  @entry["user_id"] = session["user_id"]
-
-  @entry.save
-
-  if params["image"] != nil
-
-    @entry.image.attach(params["image"])
-
+  def new
   end
 
-  redirect_to "/places/#{@entry["place_id"]}"
+  def create
+
+    @entry = Entry.new
+
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["occurred_on"] = params["occurred_on"]
+    @entry["place_id"] = params["place_id"]
+    @entry["user_id"] = session["user_id"]
+
+    @entry.save
+
+    if params["image"] != nil
+
+      @entry.image.attach(params["image"])
+
+    end
+
+    redirect_to "/places/#{@entry["place_id"]}"
+
+  end
 
 end
